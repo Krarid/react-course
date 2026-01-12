3d5653f8e0bb7efb8d21d2c04b959f3c0a97a576
Javier Meléndez <jmz.krarid@outlook.com>
2026-01-12 15:15:07 -0600

    feat: add previous searches by entering or clicking search button

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -12,13 +12,23 @@ export const GifsApp = () => {
         console.log(term);
     }
 
+    const handleSearch = (term: string): void => {
+        if (term.length === 0)
+            return;
+
+        if (previousTerms.includes(term))
+            return;
+
+        setPreviousTerms([...previousTerms, term]);
+    }
+
     return (
         <>
             {/* Header */}
             <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el gif perfecto" />
 
             {/* Search */}
-            <SearchBar placeholder='buscar gifs' />
+            <SearchBar placeholder='buscar gifs' onSearch={handleSearch} />
 
             {/* Busquedas previas */}
             <PreviousSearches searches={previousTerms} onTermClicked={handleTermClicked} />
--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
+
 interface Props {
-    placeholder?: string
+    placeholder?: string;
+    onSearch: (query: string) => void;
 }
 
-export const SearchBar = ({ placeholder = 'buscar gifs' }: Props) => {
+export const SearchBar = ({ placeholder = 'buscar gifs', onSearch }: Props) => {
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            onSearch(query);
+            setQuery('');
+        }, 700);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [query, onSearch]);
+
+    const handleQuery = () => {
+        onSearch(query);
+        setQuery('');
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            handleQuery();
+        }
+    }
+
     return (
         <div className="search-container">
-            <input type="text" placeholder={placeholder} />
-            <button>Buscar</button>
+            <input id="searchBar" type="text" placeholder={placeholder} value={query}
+                onChange={(event) => { setQuery(event.target.value); }}
+                onKeyDown={handleKeyDown} />
+            <button onClick={handleQuery}>Buscar</button>
         </div>
     )
 }
